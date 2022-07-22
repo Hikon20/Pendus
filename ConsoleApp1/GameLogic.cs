f97f91d61fa7ff014614a17b2b1951a2ce9a4ada
H <H@DESKTOP-IDHVV3C>
2022-07-22 20:45:25 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/GameLogic.cs
@@ -0,0 +1,73 @@
+﻿namespace ConsoleApp1
+{
+    public class GameLogic
+    {
+        public byte FailAttemp { get; set; }
+        public byte MaxTry { get; }
+        public string WordToFind { get; set; }
+        public bool HasLost { get; set; }
+        public bool HaveWin { get; set; }
+        public bool End { get; set; }
+        public List<char> LetterFound { get; set; }
+        public GameLogic()
+        {
+            LetterFound = new List<char>();
+            MaxTry = 5;
+            WordToFind = "manger".ToLower();
+        }
+
+        public void OutputWord()
+        {
+            string outputWordFinal = string.Empty;
+            for (byte i = 0; i != WordToFind.Length; i++)
+            {
+                if (LetterFound.Contains(WordToFind[i]))
+                {
+                    outputWordFinal += WordToFind[i];
+                }
+                else
+                {
+                    outputWordFinal += " _";
+                }
+            }
+            Console.WriteLine(outputWordFinal);
+        }
+        public void OutputWordLenght()
+        {
+            Console.WriteLine("The words containt {0} letter", WordToFind.Length);
+        }
+
+        public void CheckWord(char e)
+        {
+            bool returnOutput = WordToFind.Contains(e);
+            if (returnOutput)
+            {
+                if (!LetterFound.Contains(e))
+                {
+                    LetterFound.Add(e);
+                    Console.WriteLine("good letter");
+                    OutputWord();
+                }
+
+                if (LetterFound.Count == WordToFind.Length)
+                {
+                    Console.WriteLine("Congratulation you found the Word");
+                    OutputWord();
+                    End = true;
+                    HaveWin = true;
+                }
+                return;
+            }
+
+            FailAttemp++;
+            if (FailAttemp >= MaxTry)
+            {
+                HasLost = true;
+                End = true;
+                Console.WriteLine("loser you lost");
+            }
+            Console.WriteLine($"wrong letter, you have {MaxTry - FailAttemp} left try");
+            OutputWord();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static GameLogic Logic = new GameLogic();
+
+        public static void Main(string[] args)
+        {
+            Logic.OutputWord();
+            Logic.OutputWordLenght();
+            while (!Logic.End)
+            {
+                Console.WriteLine("Print one letter");
+                char letter = char.ToLower(Console.ReadKey(true).KeyChar);
+                Logic.CheckWord(letter);
+            }
+            Console.Read();
+        }
+    }
+}
